93925b2c68247fd8faba2234dd03a2b4c6523d29
Anshu <anshuaman9999@gmail.com>
2024-12-23 00:11:31 +0530

    fix : dashboard widget

--- src/components/dashboard/DashboardWidgets.tsx
+++ src/components/dashboard/DashboardWidgets.tsx
@@ -1,20 +1,22 @@
-// dashboardWidget component
-import React, { useEffect, useState } from 'react';
-import { Row, Col, Typography, Card } from 'antd';
-import { BarChartOutlined } from '@ant-design/icons';
-import '../../styles/DashboardWidgets.scss';
-import { Link } from 'react-router-dom';
-import { dashboardCategories, orderStatusTypesData } from '../../utils/constants';
-import { useLoader } from '../../context/LoaderContext';
-import { orderApi } from '../../services/apis/orderApi';
+import React, { useEffect, useState } from "react";
+import { Row, Col, Typography, Card } from "antd";
+import { BarChartOutlined } from "@ant-design/icons";
+import "../../styles/DashboardWidgets.scss";
+import { Link } from "react-router-dom";
+import {
+  dashboardCategories,
+  orderStatusTypesData,
+} from "../../utils/constants";
+import { useLoader } from "../../context/LoaderContext";
+import { orderApi } from "../../services/apis/orderApi";
 
 export interface Widget {
   title: string;
   firstCount: number;
-  secondCount: number;
-  secondCountPrefix: string;
-  secondCountSuffix: string;
-  secondCountType: 'success' | 'danger' | 'warning' | 'secondary';
+  secondCount?: number;
+  secondCountPrefix?: string;
+  secondCountSuffix?: string;
+  secondCountType?: "success" | "danger" | "warning" | "secondary";
   link: string;
 }
 
@@ -30,38 +32,69 @@ interface Props {
   selectedDateRange: [string, string] | null;
 }
 
-const DashboardWidgets: React.FC <Props> = ({selectedDateRange}) => {
+const DashboardWidgets: React.FC<Props> = ({ selectedDateRange }) => {
   const [ordersCount, setOrdersCount] = useState({
     Pending: 0,
     Completed: 0,
     Cancelled: 0,
     Total: 0,
   });
+
+  const [maaliCount, setMaaliCount] = useState({
+    Subscriptions: 0,
+    OneTime: 0,
+    Weekly: 0,
+    Monthly: 0,
+  });
+
   const { startLoader, stopLoader } = useLoader();
   const [dashboardCount, setDashboardCount] = useState(dashboardCategories);
 
   useEffect(() => {
-    fetchOrderCount(selectedDateRange);
+    fetchOrderAndBookingCounts(selectedDateRange);
   }, [selectedDateRange]);
 
-  const fetchOrderCount = async (selectedDateRange: any) => {
+  const fetchOrderAndBookingCounts = async (selectedDateRange: any) => {
     try {
       startLoader();
-      const orderResponse = await orderApi.getAllOrdersCount(selectedDateRange);
-      if (orderResponse.data.ok) {
-        const data = orderResponse?.data?.orderCounts;
-        const pendingCounts = data?.[orderStatusTypesData.Processing.title] || 0;
-        const completedCounts = data?.[orderStatusTypesData.Completed.title] || 0;
-        const cancelledCounts = data?.[orderStatusTypesData.Cancelled.title] || 0;
+      const response = await orderApi.getAllOrdersCount(selectedDateRange);
+      if (response.data.ok) {
+        const data = response?.data;
+
+        // Extract order counts
+        const orderCounts = data?.orderCounts || {};
+        const pendingCounts =
+          orderCounts?.[orderStatusTypesData.Processing.title] || 0;
+        const completedCounts =
+          orderCounts?.[orderStatusTypesData.Completed.title] || 0;
+        const cancelledCounts =
+          orderCounts?.[orderStatusTypesData.Cancelled.title] || 0;
+
+        // Extract booking counts
+        const bookingCounts = data?.bookingCounts || {};
+        const oneTimeSubscriptions = bookingCounts?.one_time || 0;
+        const weeklySubscriptions = bookingCounts?.weekly || 0;
+        const monthlySubscriptions = bookingCounts?.monthly || 0;
+
+        // Update orders count
         setOrdersCount({
           Pending: pendingCounts,
           Completed: completedCounts,
           Cancelled: cancelledCounts,
           Total: pendingCounts + completedCounts + cancelledCounts,
         });
+
+        // Update Maali count
+        setMaaliCount({
+          Subscriptions:
+            oneTimeSubscriptions + weeklySubscriptions + monthlySubscriptions,
+          OneTime: oneTimeSubscriptions,
+          Weekly: weeklySubscriptions,
+          Monthly: monthlySubscriptions,
+        });
       }
     } catch (err) {
-      console.error('Error fetching the order count:', err);
+      console.error("Error fetching the order and booking counts:", err);
     } finally {
       stopLoader();
     }
@@ -70,17 +103,54 @@ const DashboardWidgets: React.FC <Props> = ({selectedDateRange}) => {
   useEffect(() => {
     setDashboardCount((prevDashboardCount) => {
       const updatedDashboardCount = [...prevDashboardCount];
-      const widgets = [...updatedDashboardCount[0].widgets];
-      if (widgets.length > 3) {
-        widgets[0] = { ...widgets[0], firstCount: ordersCount.Total };
-        widgets[1] = { ...widgets[1], firstCount: ordersCount.Pending };
-        widgets[2] = { ...widgets[2], firstCount: ordersCount.Completed };
-        widgets[3] = { ...widgets[3], firstCount: ordersCount.Cancelled };
+
+      // Update Orders Widgets
+      const orderWidgets = [...updatedDashboardCount[0].widgets];
+      if (orderWidgets.length > 3) {
+        orderWidgets[0] = { ...orderWidgets[0], firstCount: ordersCount.Total };
+        orderWidgets[1] = {
+          ...orderWidgets[1],
+          firstCount: ordersCount.Pending,
+        };
+        orderWidgets[2] = {
+          ...orderWidgets[2],
+          firstCount: ordersCount.Completed,
+        };
+        orderWidgets[3] = {
+          ...orderWidgets[3],
+          firstCount: ordersCount.Cancelled,
+        };
       }
-      updatedDashboardCount[0] = { ...updatedDashboardCount[0], widgets };
+      updatedDashboardCount[0] = {
+        ...updatedDashboardCount[0],
+        widgets: orderWidgets,
+      };
+
+      // Update Maali Widgets
+      const maaliWidgets = [...updatedDashboardCount[1].widgets];
+      if (maaliWidgets.length > 3) {
+        maaliWidgets[0] = {
+          ...maaliWidgets[0],
+          firstCount: maaliCount.Subscriptions,
+        };
+        maaliWidgets[1] = {
+          ...maaliWidgets[1],
+          firstCount: maaliCount.OneTime,
+        };
+        maaliWidgets[2] = { ...maaliWidgets[2], firstCount: maaliCount.Weekly };
+        maaliWidgets[3] = {
+          ...maaliWidgets[3],
+          firstCount: maaliCount.Monthly,
+        };
+      }
+      updatedDashboardCount[1] = {
+        ...updatedDashboardCount[1],
+        widgets: maaliWidgets,
+      };
+
       return updatedDashboardCount;
     });
-  }, [ordersCount]);
+  }, [ordersCount, maaliCount]);
 
   return (
     <div className="dashboard-container">
@@ -98,12 +168,12 @@ const DashboardWidgets: React.FC <Props> = ({selectedDateRange}) => {
                     extra={<BarChartOutlined />}
                     style={{
                       marginBottom: 16,
-                      color: 'white', // Text color for contrast
+                      color: "white", // Text color for contrast
                     }}
                   >
                     <Row>
-                      <Col span={24} style={{ textAlign: 'center' }}>
-                        <Text strong style={{ fontSize: '1.5em' }}>
+                      <Col span={24} style={{ textAlign: "center" }}>
+                        <Text strong style={{ fontSize: "1.5em" }}>
                           {widget?.firstCount}
                         </Text>
                       </Col>
